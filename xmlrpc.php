91a213af7c8d8d46b9c54c5cefc2ed93ada85a7e
Abhay Rana <capt.n3m0@gmail.com>
2012-08-16 11:33:57 +0530

    Got hit by a php bug in 5.3.2 (phpfog) (url validation)
    - https://bugs.php.net/bug.php?id=51192
    - Was causing FILTER_URL to fail for a valid url
    - Shifted to a custom valid_url function from drupal

--- xmlrpc.php
+++ xmlrpc.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(-1);
+ini_set('display_errors',1);
 $request_body = file_get_contents('php://input');
 $xml = simplexml_load_string($request_body);
 
@@ -51,7 +53,7 @@ switch($xml->methodName)
 
 		//Make the webrequest
 		//Only if we have a valid url
-		if(filter_var($url, FILTER_VALIDATE_URL))
+		if(valid_url($url,true))
 		{
 			// Load Requests Library
 			include('requests/Requests.php');
@@ -61,7 +63,7 @@ switch($xml->methodName)
 			$response = Requests::post($url, $headers, json_encode($obj));
 
 			if($response->success)
-				success('<string>1</string>');
+				success('<string>'.$response->status_code.'</string>');
 			else
 				failure($response->status_code);
 		}
@@ -125,4 +127,29 @@ $xml= <<<EOD
 
 EOD;
 output($xml);
+}
+
+/** Used from drupal */
+function valid_url($url, $absolute = FALSE) {
+  if ($absolute) {
+    return (bool) preg_match("
+      /^                                                      # Start at the beginning of the text
+      (?:https?):\/\/                                # Look for ftp, http, https or feed schemes
+      (?:                                                     # Userinfo (optional) which is typically
+        (?:(?:[\w\.\-\+!$&'\(\)*\+,;=]|%[0-9a-f]{2})+:)*      # a username or a username and password
+        (?:[\w\.\-\+%!$&'\(\)*\+,;=]|%[0-9a-f]{2})+@          # combination
+      )?
+      (?:
+        (?:[a-z0-9\-\.]|%[0-9a-f]{2})+                        # A domain name or a IPv4 address
+        |(?:\[(?:[0-9a-f]{0,4}:)*(?:[0-9a-f]{0,4})\])         # or a well formed IPv6 address
+      )
+      (?::[0-9]+)?                                            # Server port number (optional)
+      (?:[\/|\?]
+        (?:[\w#!:\.\?\+=&@$'~*,;\/\(\)\[\]\-]|%[0-9a-f]{2})   # The path and query (optional)
+      *)?
+    $/xi", $url);
+  }
+  else {
+    return (bool) preg_match("/^(?:[\w#!:\.\?\+=&@$'~*,;\/\(\)\[\]\-]|%[0-9a-f]{2})+$/i", $url);
+  }
 }
