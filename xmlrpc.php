3b1db4af06a587eb90c6e4eaaace5a07fe9fbab0
Marcus Povey <marcus@marcus-povey.co.uk>
2012-11-06 14:36:48 +0000

    Source formatting.

--- xmlrpc.php
+++ xmlrpc.php
@@ -1,88 +1,80 @@
 <?php
 
-    require_once(dirname(__FILE__) . '/settings.php');
+require_once(dirname(__FILE__) . '/settings.php');
 
 error_reporting(-1);
-ini_set('display_errors',1);
+ini_set('display_errors', 1);
 $request_body = file_get_contents('php://input');
 $xml = simplexml_load_string($request_body);
 
-switch($xml->methodName)
-{
+switch ($xml->methodName) {
 
-	//wordpress blog verification
-	case 'mt.supportedMethods':
-		success('metaWeblog.getRecentPosts');
-		break;
-	//first authentication request from ifttt
-	case 'metaWeblog.getRecentPosts':
-		//send a blank blog response
-		//this also makes sure that the channel is never triggered
-		success('<array><data></data></array>');
-		break;
+    //wordpress blog verification
+    case 'mt.supportedMethods':
+        success('metaWeblog.getRecentPosts');
+        break;
+    //first authentication request from ifttt
+    case 'metaWeblog.getRecentPosts':
+        //send a blank blog response
+        //this also makes sure that the channel is never triggered
+        success('<array><data></data></array>');
+        break;
 
-	case 'metaWeblog.newPost':
-		//@see http://codex.wordpress.org/XML-RPC_WordPress_API/Posts#wp.newPost
-		$obj = new stdClass;
-		//get the parameters from xml
-		$obj->user = (string)$xml->params->param[1]->value->string;
-		$obj->pass = (string)$xml->params->param[2]->value->string;
+    case 'metaWeblog.newPost':
+        //@see http://codex.wordpress.org/XML-RPC_WordPress_API/Posts#wp.newPost
+        $obj = new stdClass;
+        //get the parameters from xml
+        $obj->user = (string) $xml->params->param[1]->value->string;
+        $obj->pass = (string) $xml->params->param[2]->value->string;
 
-		//@see content in the wordpress docs
-		$content = $xml->params->param[3]->value->struct->member;
-		foreach($content as $data)
-		{
-			switch((string)$data->name)
-			{
-				//we use the tags field for providing webhook URL
-				case 'mt_keywords':
-					$url = $data->xpath('value/array/data/value/string');
-					$url = (string)$url[0];
-					break;
+        //@see content in the wordpress docs
+        $content = $xml->params->param[3]->value->struct->member;
+        foreach ($content as $data) {
+            switch ((string) $data->name) {
+                //we use the tags field for providing webhook URL
+                case 'mt_keywords':
+                    $url = $data->xpath('value/array/data/value/string');
+                    $url = (string) $url[0];
+                    break;
 
-				//the passed categories are parsed into an array
-				case 'categories':
-					$categories=array();
-					foreach($data->xpath('value/array/data/value/string') as $cat)
-						array_push($categories,(string)$cat);
-					$obj->categories = $categories;
-					break;
+                //the passed categories are parsed into an array
+                case 'categories':
+                    $categories = array();
+                    foreach ($data->xpath('value/array/data/value/string') as $cat)
+                        array_push($categories, (string) $cat);
+                    $obj->categories = $categories;
+                    break;
 
-				//this is used for title/description
-				default:
-					$obj->{$data->name} = (string)$data->value->string;
-			}
-		}
+                //this is used for title/description
+                default:
+                    $obj->{$data->name} = (string) $data->value->string;
+            }
+        }
 
-		//Make the webrequest
-		//Only if we have a valid url
-		if(valid_url($url,true))
-		{
-			// Load Requests Library
-			include('requests/Requests.php');
-			Requests::register_autoloader();
+        //Make the webrequest
+        //Only if we have a valid url
+        if (valid_url($url, true)) {
+            // Load Requests Library
+            include('requests/Requests.php');
+            Requests::register_autoloader();
 
-			$headers = array('Content-Type' => 'application/json');
-			$response = Requests::post($url, $headers, json_encode($obj));
+            $headers = array('Content-Type' => 'application/json');
+            $response = Requests::post($url, $headers, json_encode($obj));
 
-			if($response->success)
-				success('<string>'.$response->status_code.'</string>');
-			else
-				failure($response->status_code);
-		}
-		else
-		{
-			//since the url was invalid, we return 400 (Bad Request)
-			failure(400);
-		}
-		
+            if ($response->success)
+                success('<string>' . $response->status_code . '</string>');
+            else
+                failure($response->status_code);
+        }
+        else {
+            //since the url was invalid, we return 400 (Bad Request)
+            failure(400);
+        }
 }
 
 /** Copied from wordpress */
-
-function success($innerXML)
-{
-	$xml =  <<<EOD
+function success($innerXML) {
+    $xml = <<<EOD
 <?xml version="1.0"?>
 <methodResponse>
   <params>
@@ -95,21 +87,21 @@ function success($innerXML)
 </methodResponse>
 
 EOD;
-	output($xml);
+    output($xml);
 }
 
-function output($xml){
-	$length = strlen($xml);
-	header('Connection: close');
-	header('Content-Length: '.$length);
-	header('Content-Type: text/xml');
-	header('Date: '.date('r'));
-	echo $xml;
-	exit;
+function output($xml) {
+    $length = strlen($xml);
+    header('Connection: close');
+    header('Content-Length: ' . $length);
+    header('Content-Type: text/xml');
+    header('Date: ' . date('r'));
+    echo $xml;
+    exit;
 }
 
-function failure($status){
-$xml= <<<EOD
+function failure($status) {
+    $xml = <<<EOD
 <?xml version="1.0"?>
 <methodResponse>
   <fault>
@@ -129,13 +121,13 @@ $xml= <<<EOD
 </methodResponse>
 
 EOD;
-output($xml);
+    output($xml);
 }
 
 /** Used from drupal */
 function valid_url($url, $absolute = FALSE) {
-  if ($absolute) {
-    return (bool) preg_match("
+    if ($absolute) {
+        return (bool) preg_match("
       /^                                                      # Start at the beginning of the text
       (?:https?):\/\/                                # Look for ftp, http, https or feed schemes
       (?:                                                     # Userinfo (optional) which is typically
@@ -151,8 +143,7 @@ function valid_url($url, $absolute = FALSE) {
         (?:[\w#!:\.\?\+=&@$'~*,;\/\(\)\[\]\-]|%[0-9a-f]{2})   # The path and query (optional)
       *)?
     $/xi", $url);
-  }
-  else {
-    return (bool) preg_match("/^(?:[\w#!:\.\?\+=&@$'~*,;\/\(\)\[\]\-]|%[0-9a-f]{2})+$/i", $url);
-  }
+    } else {
+        return (bool) preg_match("/^(?:[\w#!:\.\?\+=&@$'~*,;\/\(\)\[\]\-]|%[0-9a-f]{2})+$/i", $url);
+    }
 }
