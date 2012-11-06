446557cb6c1d94b3a8c4eb7b998266f1c7f146e3
Marcus Povey <marcus@marcus-povey.co.uk>
2012-11-06 17:05:21 +0000

    Adding RAW JSON Body plugin

---
+++ plugins/jsonbody.php
@@ -0,0 +1,24 @@
+<?php
+
+    /**
+     * JSON Body.
+     * 
+     * This plugin takes the content of the body and assumes that it is valid json, decodes it and replaces 
+     * any of the original variables passed.
+     * 
+     * This lets you mimic payloads expected by various webhook endpoints.
+     */
+    class JSONBody extends Plugin {
+        
+        public function execute($plugin, $object, $raw) {
+            
+            $json = json_decode($object->body);
+            if (!$json) {
+                __log("Invalid JSON payload", 'ERROR');
+                return false;
+            }
+            
+            return $json;
+            
+        }
+    }
